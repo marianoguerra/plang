04f1d965ac9b1b2d4d4205db27ae5b6b2c4fddec
Mariano Guerra <luismarianoguerra@gmail.com>
2013-10-03 23:14:26 +0000

    lookup symbols in environment

--- plang.py
+++ plang.py
@@ -12,6 +12,27 @@ class Type(BaseBox):
     def eval(self, cc):
         return cc.resolve(self)
 
+class Error(Exception):
+    pass
+
+class UnboundVariable(Error):
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return '(UnboundVariable "%s")' % self.name
+
+class Env(Type):
+    def __init__(self, bindings):
+        self.bindings = bindings
+
+    def get(self, name):
+        result = self.bindings.get(name, None)
+
+        if result is None:
+            raise UnboundVariable(name)
+        else:
+            return result
 
 class Int(Type):
     def __init__(self, value):
@@ -65,15 +86,17 @@ class Symbol(Type):
         return '%s' % self.value
 
     def eval(self, cc):
-        return cc.resolve(Int(42))
+        name = self.value
+        return cc.resolve(cc.env.get(name))
 
 true = Bool(True)
 false = Bool(False)
 
 class Cc(Type):
-    def __init__(self, value, cont):
+    def __init__(self, value, cont, env):
         self.value = value
         self.cont = cont
+        self.env = env
 
     def run(self):
         return self.value.eval(self)
@@ -85,26 +108,33 @@ def print_result(result):
     print "result:", result.__str__()
 
 def entry_point(argv):
+    env = Env({"answer": Int(42)})
     val_i = Int(42)
     val_f = Float(42.1)
 
-    cci = Cc(val_i, print_result)
+    cci = Cc(val_i, print_result, env)
     cci.run()
 
-    ccf = Cc(val_f, print_result)
+    ccf = Cc(val_f, print_result, env)
     ccf.run()
 
-    ccb = Cc(true, print_result)
+    ccb = Cc(true, print_result, env)
     ccb.run()
 
-    ccs = Cc(Str("hi there"), print_result)
+    ccs = Cc(Str("hi there"), print_result, env)
     ccs.run()
 
-    cck = Cc(Keyword("hi-there"), print_result)
+    cck = Cc(Keyword("hi-there"), print_result, env)
     cck.run()
 
-    ccs = Cc(Symbol("answer"), print_result)
+    ccs = Cc(Symbol("answer"), print_result, env)
     ccs.run()
+
+    try:
+        ccs = Cc(Symbol("not_there"), print_result, env)
+        ccs.run()
+    except UnboundVariable as error:
+        print "Error:", error.__str__()
 
     return 0
 
