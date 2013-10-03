bb7bae5ac089f52310cfa6b8e39cda78c6b1b7c8
Mariano Guerra <luismarianoguerra@gmail.com>
2013-10-03 22:38:31 +0000

    eval with continuation for int

--- Makefile
+++ Makefile
@@ -3,11 +3,11 @@ all: plang-c
 
 plang-c:
 	@echo "Building Plang with JIT"
-	../../pypy/rpython/bin/rpython plang.py
+	../../pypy/pypy-c ../../pypy/rpython/bin/rpython plang.py
 
 plang-c-jit:
 	@echo "Building Plang with JIT"
-	../../pypy/rpython/bin/rpython -Ojit plang.py
+	../../pypy/pypy-c ../../pypy/rpython/bin/rpython -Ojit plang.py
 
 clean:
 	rm plang-c
--- plang.py
+++ plang.py
@@ -9,14 +9,33 @@ class Type(BaseBox):
     def __str__(self):
         return str(self.value)
 
+    def eval(self, cc):
+        return cc.resolve(self)
+
+
 class Int(Type):
     def __init__(self, value):
         Type.__init__(self)
         self.value = value
 
+class Cc(Type):
+    def __init__(self, value, cont):
+        self.value = value
+        self.cont = cont
+
+    def run(self):
+        return self.value.eval(self)
+
+    def resolve(self, value):
+        return self.cont(value)
+
+def print_result(result):
+    print "result:", result.__str__()
+
 def entry_point(argv):
     val = Int(42)
-    print "hello pypy!", val.__str__()
+    cc = Cc(val, print_result)
+    cc.run()
     return 0
 
 def target(*args):
