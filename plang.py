50fad03be57aa62f5c30c897f855a9422dce9cd9
Mariano Guerra <luismarianoguerra@gmail.com>
2013-10-13 19:24:25 +0000

    add parser, run from stdin

--- plang.py
+++ plang.py
@@ -1,263 +1,39 @@
+import os
 import sys
 
-class PError(Exception):
-    def __init__(self, msg):
-        self.msg = msg
+from ptypes import *
+import rply.parser
 
-class PUnboundError(PError):
-    def __init__(self, msg, name, env):
-        PError.__init__(self, msg)
-        self.name = name
-        self.env = env
+import edn
 
-class PCallableExpectedError(PError):
-    def __init__(self, msg, value):
-        self.msg = msg
-        self.value = value
-
-    def __str__(self):
-        return "%s: %s" % (self.msg, self.value.to_str())
-
-class Resolver(object):
-    def resolve(self, value):
-        return value
-
-class CallResolver(Resolver):
-    def __init__(self, val, cc):
-        self.val = val
-        self.cc = cc
-
-    def resolve(self, value):
-        if isinstance(value, Callable):
-            return value.call(self.val.tail, self.cc)
-        else:
-            raise PCallableExpectedError("Callable expected", value)
-
-class TailResolver(Resolver):
-    def __init__(self, val, cc):
-        self.val = val
-        self.cc = cc
-
-    def resolve(self, tail):
-        return self.cc.resolve(Pair(self.val, tail))
-
-class HeadResolver(Resolver):
-    def __init__(self, val, cc):
-        self.val = val
-        self.cc = cc
-
-    def resolve(self, head):
-        return Cc(self.val.tail, TailResolver(head, self.cc), self.cc.env,
-                False)
-
-
-class Type(object):
-    def __init__(self):
-        pass
-
-class Cc(Type):
-    def __init__(self, value, cont, env, do_run=True):
-        self.value = value
-        self.cont = cont
-        self.env = env
-        self.do_run = do_run
-
-    def resolve(self, value):
-        return self.cont.resolve(value)
-
-    def step(self):
-        return self.value.eval(self)
-
-    def run(self):
-        result = self.step()
-        while isinstance(result, Cc):
-            result = result.step()
-
-        return result
-
-class Env(Type):
-    def __init__(self, bindings):
-        self.value = None
-        self.bindings = bindings
-
-    def get(self, name):
-        # dict.get must take the two args
-        result = self.bindings.get(name, None)
-        if result is None:
-            raise PUnboundError("'%s' not bound" % name, name, self)
-
-        return result
-
-class Int(Type):
-    # if not redefined int acts as a float
-    def __init__(self, value):
-        Type.__init__(self)
-        self.value = value
-
-    def to_str(self):
-        return "%s" % self.value
-
-    def eval(self, cc):
-        return cc.resolve(self)
-
-
-class Float(Type):
-    def __init__(self, value):
-        Type.__init__(self)
-        self.value = value
-
-    def eval(self, cc):
-        return cc.resolve(self)
-
-    def to_str(self):
-        return "%s" % self.value
-
-class Bool(Type):
-    def __init__(self, value):
-        Type.__init__(self)
-        self.value = value
-
-    def eval(self, cc):
-        return cc.resolve(self)
-
-    def to_str(self):
-        if self.value:
-            return "true"
-        else:
-            return "false"
-
-class Nil(Type):
-    def __init__(self):
-        Type.__init__(self)
-        self.value = None
-
-    def eval(self, cc):
-        return cc.resolve(self)
-
-    def to_str(self):
-        return "nil"
-
-class Symbol(Type):
-    # if not redefined it doesn't compile
-    def __init__(self, value):
-        Type.__init__(self)
-        self.value = value
-
-    def eval(self, cc):
-        return cc.resolve(cc.env.get(self.value))
-
-    def to_str(self):
-        return "%s" % self.value
-
-class Str(Type):
-    def __init__(self, value):
-        Type.__init__(self)
-        self.value = value
-
-    def eval(self, cc):
-        return cc.resolve(self)
-
-    def to_str(self):
-        return '"%s"' % self.value
-
-true = Bool(True)
-false = Bool(False)
-nil = Nil()
-
-class Callable(Type):
-    def __init__(self, name):
-        self.name = name
-
-    def call(self, args, cc):
-        return cc.resolve(nil)
-
-class Operative(Callable):
-    def __init__(self, name):
-        Callable.__init__(self, name)
-
-    def call(self, args, cc):
-        return cc.resolve(nil)
-
-class Applicative(Callable):
-    def __init__(self, name):
-        Callable.__init__(self, name)
-
-    def handle(self, args, cc):
-        return cc.resolve(args)
-
-    def call(self, args, cc):
-        cc1 = Cc(nil, identity, cc.env, False)
-        eargs = args.eval(cc1).run()
-        return self.handle(eargs, cc)
-
-class OpDump(Operative):
-    def __init__(self):
-        Callable.__init__(self, "dump")
-
-    def call(self, args, cc):
-        print "dump:", args.to_str()
-        return cc.resolve(nil)
-
-class FnDisplay(Applicative):
-    def __init__(self):
-        Callable.__init__(self, "display")
-
-    def handle(self, args, cc):
-        print "display:", args.to_str()
-        return cc.resolve(nil)
-
-class Pair(Type):
-    def __init__(self, head, tail=nil):
-        self.head = head
-        self.tail = tail
-
-    def __iter__(self):
-        pair = self
-        while True:
-            cur = pair.head
-            yield cur
-            if pair.tail == nil:
+def readinput():
+    result = []
+    while True:
+        s = os.read(0, 1)
+        result.append(s)
+        if s == '':
+            if len(result) > 1:
                 break
-            else:
-                pair = pair.tail
-                if not isinstance(pair, Pair):
-                    break
-
-    def eval(self, cc):
-        if cc.do_run:
-            return Cc(self.head, CallResolver(self, cc), cc.env)
-        else:
-            return Cc(self.head, HeadResolver(self, cc), cc.env, False)
-
-    def to_str(self):
-        return "(%s)" % " ".join([item.to_str() for item in self])
-
-identity = Resolver()
+            raise SystemExit
+    return "".join(result)
 
 def entry_point(argv):
+    input_data = readinput()
+    try:
+        input_parsed = edn.loads(input_data)
+    except rply.parser.ParsingError as error:
+        pos = error.getsourcepos()
+        print "Error reading code at line: %d column: %d" % (pos.lineno, pos.colno)
+        return -1
+
     env = Env({
         "version": Str("0.0.1"),
         "dump": OpDump(),
         "display": FnDisplay()
     })
 
-    print Cc(Int(42), identity, env).step().to_str()
-    print Cc(Float(42.3), identity, env).step().to_str()
-    print Cc(true, identity, env).step().to_str()
-    print Cc(false, identity, env).step().to_str()
-    print Cc(nil, identity, env).step().to_str()
-    print Cc(Str("hello world!"), identity, env).step().to_str()
-    print Cc(Symbol("version"), identity, env).step().to_str()
-    result = Cc(Pair(Symbol("dump"), Pair(Int(42))), identity, env).run().to_str()
-    print "dump returned:", result
 
-    result1 = Cc(Pair(Symbol("dump"), Pair(Int(42), Pair(Symbol("version")))),
-            identity, env).run().to_str()
-    print "dump returned:", result1
-
-    result2 = Cc(Pair(Symbol("display"), Pair(Int(42), Pair(Symbol("version")))),
-            identity, env).run().to_str()
-    print "display returned:", result2
+    print Cc(input_parsed, identity, env).run().to_str()
 
     return 0
 
