70aba18f7fd601002fccaab018e2f414e438c897
Mariano Guerra <luismarianoguerra@gmail.com>
2013-10-13 17:08:27 +0000

    add float and bool

--- plang.py
+++ plang.py
@@ -1,7 +1,38 @@
 import sys
 
+class Type(object):
+    def __init__(self, value):
+        self.value = value
+
+    def to_str(self):
+        return "%s" % self.value
+
+class Int(Type):
+    # if not redefined int acts as a float
+    def __init__(self, value):
+        self.value = value
+
+    def to_str(self):
+        return "%s" % self.value
+
+class Float(Type):
+    pass
+
+class Bool(Type):
+    def to_str(self):
+        if self.value:
+            return "true"
+        else:
+            return "false"
+
+true = Bool(True)
+false = Bool(False)
+
 def entry_point(argv):
-    print "hello world"
+    print Int(42).to_str()
+    print Float(42.3).to_str()
+    print true.to_str()
+    print false.to_str()
     return 0
 
 def target(*args):
