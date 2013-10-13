9065fa9af2db7c3dc8a01c5e5f806bf4bf8b7bee
Mariano Guerra <luismarianoguerra@gmail.com>
2013-10-13 18:16:28 +0000

    pair eval and operatives

--- plang.py
+++ plang.py
@@ -10,9 +10,25 @@ class PUnboundError(PError):
         self.name = name
         self.env = env
 
+class PCallableExpectedError(PError):
+    def __init__(self, msg, value):
+        self.msg = msg
+        self.value = value
+
 class Resolver(object):
     def resolve(self, value):
         return value
+
+class CallResolver(Resolver):
+    def __init__(self, val, cc):
+        self.val = val
+        self.cc = cc
+
+    def resolve(self, value):
+        if isinstance(value, Callable):
+            return value.call(self.val.tail, self.cc)
+        else:
+            raise PCallableExpectedError("Callable expected", value)
 
 class Type(object):
     def __init__(self):
@@ -29,6 +45,13 @@ class Cc(Type):
 
     def step(self):
         return self.value.eval(self)
+
+    def run(self):
+        result = self.step()
+        while isinstance(result, Cc):
+            result = result.step()
+
+        return result
 
 class Env(Type):
     def __init__(self, bindings):
@@ -119,9 +142,54 @@ true = Bool(True)
 false = Bool(False)
 nil = Nil()
 
+class Callable(Type):
+    def __init__(self, name):
+        self.name = name
+
+    def call(self, args, cc):
+        return cc.resolve(nil)
+
+class Operative(Callable):
+    def __init__(self, name):
+        Callable.__init__(self, name)
+
+    def call(self, args, cc):
+        return cc.resolve(nil)
+
+class OpDump(Operative):
+    def __init__(self):
+        Callable.__init__(self, "dump")
+
+    def call(self, args, cc):
+        print "dump:", args.to_str()
+        return cc.resolve(nil)
+
+class Pair(Type):
+    def __init__(self, head, tail=nil):
+        self.head = head
+        self.tail = tail
+
+    def __iter__(self):
+        pair = self
+        while True:
+            cur = pair.head
+            yield cur
+            if pair.tail == nil:
+                break
+            else:
+                pair = pair.tail
+                if not isinstance(pair, Pair):
+                    break
+
+    def eval(self, cc):
+        return Cc(self.head, CallResolver(self, cc), cc.env)
+
+    def to_str(self):
+        return "(%s)" % " ".join([item.to_str() for item in self])
+
 def entry_point(argv):
     identity = Resolver()
-    env = Env({"version": Str("0.0.1")})
+    env = Env({"version": Str("0.0.1"), "dump": OpDump()})
 
     print Cc(Int(42), identity, env).step().to_str()
     print Cc(Float(42.3), identity, env).step().to_str()
@@ -130,6 +198,8 @@ def entry_point(argv):
     print Cc(nil, identity, env).step().to_str()
     print Cc(Str("hello world!"), identity, env).step().to_str()
     print Cc(Symbol("version"), identity, env).step().to_str()
+    result = Cc(Pair(Symbol("dump"), Int(42)), identity, env).run().to_str()
+    print "dump returned:", result
 
     return 0
 
